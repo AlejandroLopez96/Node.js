aa2708ccc0e6a84d5ba3162d152d4c6168508221
AlejandroLopez96 <alexlopezortiz9@gmail.com>
2017-02-03 10:57:49 +0100

    Plantillas Jade Express de NodeJS

--- express-jade-jueves/index.js
+++ express-jade-jueves/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 var persona = {
     nombre: "Aitor",
@@ -25,7 +27,7 @@ app.get('/usuarios', function (req, res) {
 });
 
 app.get('/saluda/:nombre', function (req, res) {
-    var datos = {
+    let datos = {
         "nombre": req.params.nombre
     };
     res.render('saluda',persona);
@@ -33,6 +35,15 @@ app.get('/saluda/:nombre', function (req, res) {
 
 app.get('/formulario', function (req, res) {
   res.render('formulario',persona);
+});
+
+app.post('/registro', function (req, res) {
+  console.log(req.body.usuario);
+  let datos = {
+    "usuario": req.body.usuario,
+    "contrasena": req.body.contrasena
+  };
+  res.render('registro',datos);
 });
 
 app.listen(8080);
